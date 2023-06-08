20162eb05342b1013dd63cdfd018e22a666872c7
Fatima Shibli <95264693+FatimaShibli@users.noreply.github.com>
2023-06-08 12:09:05 -0500

    Update StarRating.js

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,56 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-
 const StarRating = () => {
-    const [rating, setRating] = useState(0);
-    const [totalRatings, setTotalRatings] = useState(0);
-  
-    useEffect(() => {
-      const fetchTotalRatings = async () => {
-        try {
-          const response = await axios.get('/api/total-ratings');
-          setTotalRatings(response.data.totalRatings);
-        } catch (error) {
-          console.error('Error occurred while fetching total ratings:', error);
-        }
-      };
-  
-      fetchTotalRatings();
-    }, []);
-  
-    const handleRatingChange = async (newRating) => {
-      setRating(newRating);
+  const [controlledRating, setControlledRating] = useState(0);
+  const [readOnlyRating, setReadOnlyRating] = useState(0);
+  const [disabledRating, setDisabledRating] = useState(0);
+  const [noRatingGiven, setNoRatingGiven] = useState(0);
+  const [totalRatings, setTotalRatings] = useState(0);
+
+  useEffect(() => {
+    const fetchTotalRatings = async () => {
       try {
-        await axios.post('/api/ratings', { rating: newRating });
-        setTotalRatings((prevTotalRatings) => prevTotalRatings + 1);
+        const response = await axios.get('/api/total-ratings');
+        setTotalRatings(response.data.totalRatings);
       } catch (error) {
-        console.error('Error occurred while saving the rating:', error);
+        console.error('Error occurred while fetching total ratings:', error);
       }
     };
-  
+
+    fetchTotalRatings();
+  }, []);
+
+  const handleRatingChange = async (newRating, setRating) => {
+    setRating(newRating);
+    try {
+      await axios.post('/api/ratings', { rating: newRating });
+      setTotalRatings((prevTotalRatings) => prevTotalRatings + 1);
+    } catch (error) {
+      console.error('Error occurred while saving the rating:', error);
+    }
+  };
+
   return (
     <div>
-      <h3>Rate this widget:</h3>
+      <h3>Controlled:</h3>
       <div>
         {[1, 2, 3, 4, 5].map((value) => (
           <span
             key={value}
-            onClick={() => handleRatingChange(value)}
+            onClick={() => handleRatingChange(value, setControlledRating)}
             style={{
               cursor: 'pointer',
-              color: value <= rating ? 'orange' : 'gray',
+              color: value <= controlledRating ? 'orange' : 'gray',
             }}
           >
             ★
           </span>
         ))}
       </div>
-      <h3>Your rating: {rating}</h3>
+      <h4>Your rating: {controlledRating}</h4>
+
+      <h3>Read Only:</h3>
+      <div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <span
+            key={value}
+            onClick={() => handleRatingChange(value, setReadOnlyRating)}
+            style={{
+              cursor: 'pointer',
+              color: value <= readOnlyRating ? 'orange' : 'gray',
+            }}
+          >
+            ★
+          </span>
+        ))}
+      </div>
+      <h4>Your rating: {readOnlyRating}</h4>
+
+      <h3>Disabled:</h3>
+      <div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <span
+            key={value}
+            onClick={() => handleRatingChange(value, setDisabledRating)}
+            style={{
+              cursor: 'pointer',
+              color: value <= disabledRating ? 'orange' : 'gray',
+            }}
+          >
+            ★
+          </span>
+        ))}
+      </div>
+      <h4>Your rating: {disabledRating}</h4>
+
+      <h3>No rating given:</h3>
+      <div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <span
+            key={value}
+            onClick={() => handleRatingChange(value, setNoRatingGiven)}
+            style={{
+              cursor: 'pointer',
+              color: value <= noRatingGiven ? 'orange' : 'gray',
+            }}
+          >
+            ★
+          </span>
+        ))}
+      </div>
+      <h4>Your rating: {noRatingGiven}</h4>
     </div>
   );
-  };
-  
+};
 
 export default StarRating;
