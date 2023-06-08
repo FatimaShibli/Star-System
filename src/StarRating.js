ba7e535ea06a2f9b705184a7b7489368b9f2d663
breannakchi <89543173+breannakchi@users.noreply.github.com>
2023-06-08 11:48:46 -0700

    Update StarRating.js with average function

--- src/StarRating.js
+++ src/StarRating.js
@@ -6,9 +6,9 @@ const StarRating = () => {
   const [readOnlyRating, setReadOnlyRating] = useState(0);
   const [disabledRating, setDisabledRating] = useState(0);
   const [noRatingGiven, setNoRatingGiven] = useState(0);
-  const [ratings, setRatings] = useState([]);
   const [totalRatings, setTotalRatings] = useState(0);
-  const [averageRating, setAverageRating] = useState(0);
+  const [ratings, setRatings] = useState([]); // New state for ratings
+  const [averageRating, setAverageRating] = useState(0); // State for average rating
 
   useEffect(() => {
     const fetchTotalRatings = async () => {
@@ -25,19 +25,30 @@ const StarRating = () => {
 
   useEffect(() => {
     const calculateAverageRating = () => {
-      const sum = ratings.reduce((acc, rating) => acc + rating, 0);
-      const average = sum / ratings.length || 0;
+      const sum =
+        controlledRating * (controlledRating > 0 ? 1 : 0) +
+        readOnlyRating * (readOnlyRating > 0 ? 1 : 0) +
+        disabledRating * (disabledRating > 0 ? 1 : 0) +
+        noRatingGiven * (noRatingGiven > 0 ? 1 : 0);
+
+      const count =
+        (controlledRating > 0 ? 1 : 0) +
+        (readOnlyRating > 0 ? 1 : 0) +
+        (disabledRating > 0 ? 1 : 0) +
+        (noRatingGiven > 0 ? 1 : 0);
+
+      const average = sum / count || 0;
       setAverageRating(average.toFixed(1));
     };
 
     calculateAverageRating();
-  }, [ratings]);
+  }, [controlledRating, readOnlyRating, disabledRating, noRatingGiven]);
 
-  const handleRatingChange = async (newRating, setRating) => {
+  const handleRatingChange = async (newRating, setRating, ratingType) => {
     setRating(newRating);
     try {
       await axios.post('/api/ratings', { rating: newRating });
-      setRatings((prevRatings) => [...prevRatings, newRating]);
+      setRatings((prevRatings) => [...prevRatings, { rating: newRating, type: ratingType }]);
       setTotalRatings((prevTotalRatings) => prevTotalRatings + 1);
     } catch (error) {
       console.error('Error occurred while saving the rating:', error);
@@ -45,6 +56,7 @@ const StarRating = () => {
   };
 
   return (
+
     <div>
       <h3>Controlled:</h3>
       <div>
@@ -114,7 +126,7 @@ const StarRating = () => {
       </div>
       <h4>Your rating: {noRatingGiven}</h4>
 
-      <h3>Average Rating: {averageRating}</h3>
+      <h3>Average of ratings: {averageRating}</h3>
     </div>
   );
 };
